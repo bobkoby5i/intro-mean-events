fda315a85c8583531607d520ea397c64d0330ab6
koby5i <bobkoby5i@xdeq.com>
2020-05-24 14:05:36 +0200

    add API register user user

--- angularauth/server/routes/api.js
+++ angularauth/server/routes/api.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-
+const User = require('../models/user')
 const mongoose = require('mongoose')
 const db = "mongodb://localhost:27017/eventsdb"
 //const db = "mongodb+srv://{user}:{password}@koby5icluster01-tlx1x.mongodb.net/test?retryWrites=true&w=majority"
@@ -19,5 +19,18 @@ router.get('/', (req, res) => {
     res.send('From API route')
 })
 
+
+router.post('/register', (req, res) => {
+    let userData = req.body
+    let user =  new User(userData)
+    user.save((error, registeredUser) => {
+        if (error) {
+            console.log(error)
+        }
+        else {
+            res.status(200).send(registeredUser)
+        }
+    }) 
+})
 
 module.exports = router
