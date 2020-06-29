8e9400fa5005115bb5eb70e8939b6a65be292489
koby5i <bobkoby5i@xdeq.com>
2020-06-29 23:38:07 +0200

    FE Angular and register from

---
+++ angularauth/ngApp/src/app/register/register.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+
+  registerUserData = {email:"", password:""}
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  registerUser() {
+    console.log(this.registerUserData)
+  }
+
+}
--- angularauth/server/routes/api.js
+++ angularauth/server/routes/api.js
@@ -27,10 +27,68 @@ router.post('/register', (req, res) => {
         if (error) {
             console.log(error)
         }
+
         else {
             res.status(200).send(registeredUser)
         }
     }) 
 })
+
+router.post('/login', (req, res) => {
+    let userData = req.body
+
+    User.findOne({email: userData.email}, (error, user) => {
+        if (error) {
+            console.log(error)
+        } else {
+            if (!user) {
+                res.status(401).send('email not recognised')
+            } else {
+                if (user.password !== userData.password) {
+                    res.status(401).send('Invalid Password')
+                } else {
+                    res.status(200).send(user)
+                }
+            }
+        }
+    } )
+})
+
+router.get('/events', (req,res) => {
+    let events = [
+        {
+            "_id": "1",
+            "name": "Auto Expo",
+            "description": "lorem ipsum",
+            "date": "2012-04-23T18:25:43.123Z"
+        },
+        {
+            "_id": "2",
+            "name": "Auto Expo",
+            "description": "lorem ipsum",
+            "date": "2012-04-23T18:25:43.123Z"
+        }
+    ]
+    res.json(events)
+})
+
+router.get('/special', (req,res) => {
+    let events = [
+        {
+            "_id": "1",
+            "name": "Auto Expo",
+            "description": "lorem ipsum",
+            "date": "2012-04-23T18:25:43.123Z"
+        },
+        {
+            "_id": "2",
+            "name": "Auto Expo",
+            "description": "lorem ipsum",
+            "date": "2012-04-23T18:25:43.123Z"
+        }
+    ]
+    res.json(events)
+})
+
 
 module.exports = router
