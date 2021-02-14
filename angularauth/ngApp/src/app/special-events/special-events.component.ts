509299bc7b2de54796f493bba5925a07a82435f0
koby5i <bobkoby5i@xdeq.com>
2021-02-14 14:04:27 +0100

    add Events UI: (html & compnents) for (events & specialEvents)

--- angularauth/ngApp/src/app/special-events/special-events.component.ts
+++ angularauth/ngApp/src/app/special-events/special-events.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { EventService } from '../event.service'; //add
 
 @Component({
   selector: 'app-special-events',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SpecialEventsComponent implements OnInit {
 
-  constructor() { }
+  specialEvents = [] // to store results
+  constructor(private _eventService: EventService) { }  //inject service 
 
   ngOnInit(): void {
+    this._eventService.getSpecialEvents() // call service
+      .subscribe(                  // subscribe to get response
+        res => this.specialEvents = res,  // store results in events[]
+        err => console.log(err)    // or log error in console
+      )
+
   }
 
 }
+
+
