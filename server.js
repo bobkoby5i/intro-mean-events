5f4d4155b49596227d5f734378a90798a1617471
koby5i <bobkoby5i@xdeq.com>
2021-02-16 00:35:55 +0100

    hello from NodeJS server on heroku

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json())
 
 app.use('/api', api)
 app.get('/', function(req, res){
-    res.send('Hello from NodeJS Server')
+    res.send('Hello from NodeJS Server on Heroku')
 })
 
 app.listen(PORT, function(){
