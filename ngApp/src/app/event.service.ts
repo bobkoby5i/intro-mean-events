43b249d6f07dc2fd3f3a7fb4d9d5a7954ed89fc7
koby5i <bobkoby5i@xdeq.com>
2021-02-16 02:12:05 +0100

    prod build

--- ngApp/src/app/event.service.ts
+++ ngApp/src/app/event.service.ts
@@ -1,13 +1,17 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from '../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EventService {
 
-  private _eventsUrl = "http://localhost:3000/api/events";
-  private _specialEventsUrl = "http://localhost:3000/api/special";
+  baseUrl = environment.baseUrl;
+
+  
+  private _eventsUrl = this.baseUrl + "/api/events";
+  private _specialEventsUrl = this.baseUrl  + "/api/special";
   constructor(private http: HttpClient) { }
 
   getEvents() {
--- ngApp/src/environments/environment.prod.ts
+++ ngApp/src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  baseUrl: 'https://koby5i-mean-intro-events.herokuapp.com'
 };
