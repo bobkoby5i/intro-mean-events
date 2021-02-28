c5727aeb775e93337352f72dd0795418e0cb5e7a
koby5i <bobkoby5i@xdeq.com>
2021-02-28 19:53:48 +0100

    remove /public ng bubild /dist/ng-app

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express()
 app.use(cors())
 
 app.use(bodyParser.json())
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/ngApp/dist/ng-app'));
 
 app.use('/api', api)
 app.get('/hello', function(req, res){
